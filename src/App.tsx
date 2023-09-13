1e1cb27ab153fdca53e4070bc5cdc3e9f9de5070
Mcode2020 <dev.mcodeinfosoft@gmail.com>
2023-09-13 20:42:10 +0530

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
+  const [style, setStyle] = useState({
+    display: "none"
+  });
+  const divRef = React.createRef<HTMLDivElement>();
+
+  const handleKeyUp = (event: any) => {
+    // Handle key up event here
+    console.log('Key up event:', event.key, event, divRef.current?.innerText);
+  };
+
+  const handleKeyDown = (event: any) => {
+    // Handle key down event here
+    console.log('Key down event:', event.key);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        
+        <div className="container">
+        <div className="payroll" contentEditable={true}
+        onKeyUp={handleKeyUp}
+        onKeyDown={handleKeyDown}
+        ref={divRef}
         >
-          Learn React
-        </a>
-      </header>
+          <div className='payroll-tags'>
+          <span className="tags-detail"><span className="inline-block whitespace-pre">Payroll Bonus COGS</span>       
+          <span className="variable-tag-argument">[x]</span></span>
+          </div>
+          <div className='tag_links'><span>&</span></div>
+          
+          <div className='payroll-tags'>
+          <span className="tags-detail"><span className="inline-block whitespace-pre">Payroll Bonus COGS</span>       
+          <span className="variable-tag-argument">[x]</span></span>
+          </div>
+          <div className='payroll-tags'>
+          <span className="tags-detail"><span className="inline-block whitespace-pre">Payroll Bonus COGS</span>       
+          <span className="variable-tag-argument">[x]</span></span>
+          </div>
+          <div className='tag_links'><span>*</span></div>
+          <div className='payroll-tags'>
+          <span className="tags-detail"><span className="inline-block whitespace-pre">Payroll Bonus COGS</span>       
+          <span className="variable-tag-argument">[x]</span></span>
+          </div>
+
+          <div className='tag-tooltip' style={style}>
+            <ul>
+              <li><div className='pay-bonus'>
+                <div className='pay-bonus-text'><p>Payroll Bonus COGS</p></div>
+                <div className='pay-currency'><p>currency</p></div></div>
+                <div className='pay-dept'><p>Payroll bonus for COGS department</p></div>
+              </li>
+              <li><div className='pay-bonus'>
+                <div className='pay-bonus-text'><p>Payroll Bonus COGS</p></div>
+                <div className='pay-currency'><p>currency</p></div></div>
+                <div className='pay-dept'><p>Payroll bonus for COGS department</p></div>
+              </li>
+              <li><div className='pay-bonus'>
+                <div className='pay-bonus-text'><p>Payroll Bonus COGS</p></div>
+                <div className='pay-currency'><p>currency</p></div></div>
+                <div className='pay-dept'><p>Payroll bonus for COGS department</p></div>
+              </li>
+              <li><div className='pay-bonus'>
+                <div className='pay-bonus-text'><p>Payroll Bonus COGS</p></div>
+                <div className='pay-currency'><p>currency</p></div></div>
+                <div className='pay-dept'><p>Payroll bonus for COGS department</p></div>
+              </li>
+            </ul>
+          </div>
+        </div>
+    </div>
     </div>
   );
 }
